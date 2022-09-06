6150ef35c917fd6b7b491b207820d993e74b8413
Himanshu <himanshu_196018@saitm.org>
2022-09-06 14:12:36 +0530

    navbar functionality added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,22 +7,27 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
+const activeLink =
+  "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+const normalLink =
+  "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
 
-  const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
-  const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
-
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:overflow-auto pb-10">
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {setActiveMenu(true)}}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
@@ -31,7 +36,9 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {setActiveMenu((prevActiveMenu) => !prevActiveMenu)}}
+                onClick={() => {
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu);
+                }}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -39,30 +46,27 @@ const Sidebar = () => {
             </TooltipComponent>
           </div>
           <div className="mt-10">
-            {links.map((item) => {
-              return (
+            {links.map((item) => (
                 <div key={item.title}>
-                  <p className="text-gray-400 m-3 mt-4 uppercase">
+                  <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
                     {item.title}
                   </p>
-                  {item.links.map((link) => {
-                    return (
+                  {item.links.map((link) => (
                       <NavLink
                         to={`/${link.name}`}
-                        onClick={() => {}}
+                        onClick=""
                         className={({ isActive }) =>
                           isActive ? activeLink : normalLink
-                        }>
-                          {link.icon}
-                          <span className="capitalize">
-                            {link.name}
-                          </span>
-                        </NavLink>
-                    );
-                  })}
+                        }
+                      >
+                        {link.icon}
+                        <span className="capitalize">{link.name}</span>
+                      </NavLink>
+                    )
+                  )}
                 </div>
-              );
-            })}
+              )
+            )}
           </div>
         </>
       )}
