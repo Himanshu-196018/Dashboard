afa8e71fe188bfd03b49dae86aa5d10b5bcf3a14
Himanshu <himanshu_196018@saitm.org>
2022-09-06 22:06:56 +0530

    commerce page design completed

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const Sparkline = () => {
+const Sparkline = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <div>Sparkline</div>
-  )
-}
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      xName="x"
+      yName="yval"
+      type={type}
+      dataSource={data}
+      tooltipSettings={{
+        visible: true,
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
+};
 
-export default Sparkline
+export default Sparkline;
